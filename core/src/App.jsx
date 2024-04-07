dd463c9b50ac052a821f67c752d85a9e6d294d2f
Mohamed Hashemi <dothashemi@gmail.com>
2024-04-07 18:19:32 +0330

    initial commit

---
+++ core/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Tic-Tac-Toe</div>
+    </>
+  );
+}
+
+export default App;
